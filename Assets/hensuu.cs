3e9eae49c91072d1fa90f8d936afd162253e0e21
D301-06 <tnk.dat.class@gmail.com>
2021-06-03 12:21:58 +0900

    ボタン

--- Assets/hensuu.cs
+++ Assets/hensuu.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class hensuu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int a = 10;
+    int vx = 20;
+
+    public void Ensyu71()
     {
-        int a = 10;
+        int vx = 5;
+        Debug.Log(vx);
+
+        vx = 10;
+        Debug.Log(vx);
+    }
+
+    public void Ensyu72()
+    {
+        Debug.Log(vx);
+    }
+
+    public void Ensyu74()
+    {
+        vx += 10;
+        Debug.Log(vx); 
+    }
+
+
+    // Start is called before the first frame update
+    public void Proc()
+    {
+        
         float fw = 3.14f;
         string str = "こんにちは";
         bool bl = true;
@@ -18,9 +42,13 @@ public class hensuu : MonoBehaviour
         Debug.Log(bl);
     }
 
+    public void Proc2()
+    {
+        Debug.Log(a);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
