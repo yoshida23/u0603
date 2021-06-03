bf00942906bf2e50f1403809c12c28dd09274bf5
D301-06 <tnk.dat.class@gmail.com>
2021-06-03 11:07:20 +0900

    Initial commit

---
+++ Assets/hensuu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hensuu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 10;
+        float fw = 3.14f;
+        string str = "こんにちは";
+        bool bl = true;
+
+        Debug.Log(a+a);
+        Debug.Log(fw);
+        Debug.Log(str);
+        Debug.Log(bl);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
